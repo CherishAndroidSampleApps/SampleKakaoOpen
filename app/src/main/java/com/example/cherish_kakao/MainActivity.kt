d1f5c3989014c2d822ecea7879c75a4143c3e87e
ny2060 <dpswpfskdud@naver.com>
2020-12-29 14:57:50 +0900

    kakaoLinkApi_ny

---
+++ app/src/main/java/com/example/cherish_kakao/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.cherish_kakao
+
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.kakao.kakaolink.v2.KakaoLinkResponse
+import com.kakao.kakaolink.v2.KakaoLinkService
+import com.kakao.message.template.*
+import com.kakao.network.ErrorResult
+import com.kakao.network.callback.ResponseCallback
+import com.kakao.util.KakaoParameterException
+import com.kakao.util.helper.log.Logger
+import kotlinx.android.synthetic.main.activity_main.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        getHashKey()
+        kakaoLinkBtn.setOnClickListener {
+
+            kakaoLink()
+
+        }
+    }
+    private fun getHashKey() {
+        var packageInfo: PackageInfo? = null
+        try {
+            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
+        for (signature in packageInfo!!.signatures) {
+            try {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
+            }
+        }
+    }
+    fun kakaoLink() {
+        val params = TextTemplate.newBuilder("안녕?? 잘 지내???ㅁㄴㅇㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ",LinkObject.newBuilder().setWebUrl("https://developers.kakao.com")
+                .setMobileWebUrl("https://developers.kakao.com").build()
+        )
+
+                /*.setSocial(
+                        SocialObject.newBuilder().setLikeCount(10).setCommentCount(20)
+                                .setSharedCount(30).setViewCount(40).build()
+                )
+                .addButton(
+                        ButtonObject(
+                                "웹에서 보기",
+                                LinkObject.newBuilder().setWebUrl("https://developers.kakao.com").setMobileWebUrl(
+                                        "https://developers.kakao.com"
+                                ).build()
+                        )
+                )
+                .addButton(
+                        ButtonObject(
+                                "앱에서 보기", LinkObject.newBuilder()
+                                .setWebUrl("'https://developers.kakao.com")
+                                .setMobileWebUrl("https://developers.kakao.com")
+                                .setAndroidExecutionParams("key1=value1")
+                                .setIosExecutionParams("key1=value1")
+                                .build()
+                        )
+                )*/
+                .build()
+
+
+        val serverCallbackArgs: MutableMap<String, String> =
+                HashMap()
+        serverCallbackArgs["user_id"] = "\${current_user_id}"
+        serverCallbackArgs["product_id"] = "\${shared_product_id}"
+
+        KakaoLinkService.getInstance().sendDefault(
+                this,
+                params,
+                serverCallbackArgs,
+                object : ResponseCallback<KakaoLinkResponse?>() {
+                    override fun onFailure(errorResult: ErrorResult) {
+                        Logger.e(errorResult.toString())
+                    }
+
+                    override fun onSuccess(result: KakaoLinkResponse?) { // 템플릿 밸리데이션과 쿼터 체크가 성공적으로 끝남. 톡에서 정상적으로 보내졌는지 보장은 할 수 없다. 전송 성공 유무는 서버콜백 기능을 이용하여야 한다.
+                    }
+                })
+    }
+}
